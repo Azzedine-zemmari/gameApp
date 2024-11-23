b82198bd5d82c51d81a8bbdf9938f6814b2a7703
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-11-23 14:50:14 +0100

    add a discover part

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,11 @@ module.exports = {
     extend: {
       colors:{
         GreenPrimary:"#2F4B4D",
+        secondery : "rgb(155, 96, 85)"
       },
       fontFamily:{
-        primary:["Inter", "Inter Placeholder", 'sans-serif']
+        primary:["Inter", "Inter Placeholder", 'sans-serif'],
+        secondery:["Instrument Serif", "Instrument Serif Placeholder", 'serif']
       }
     },
   },
