1b7fbe005e91e94ca29d26c529cd63e0aaac7ded
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-11-23 16:44:38 +0100

    add a tag element

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        id:1,
+        city:"MARRAKECH",
+        name:"Shtatto",
+        tags:["Coffee","Restaurants"],
+        description:"Elevate your Marrakech experience at Shtatto, a rooftop restaurant bar nestled in the vibrant heart of the Medina. Immerse yourself in a world of positive vibes and breathtaking sunsets as you discover our high-altitude oasis with panoramic views over the city. Our culinary journey combines the rich flavors of Moroccan and international cuisines, prepared with the freshest produce, making every dish a delectable masterpiece. Join us for unforgettable moments with friends and savor the enchanting spectacle of the sunset. Let yourself be captivated by this unique and extraordinary experience in Marrakech!",
+        tumbnail:"./src/image/shatato.avif",
+        images:["./src/image/shatao1.avif","./src/image/shatato2.avif","./src/image/shatato3.avif"],
+        instagramLink:"https://www.instagram.com/shtattomarrakech/",
+        maplink:"https://www.google.com/maps/place/SHTATTO/@31.6293186,-7.9865762,15z/data=!4m6!3m5!1s0xdafee69ff8c925d:0xd146608f5cd420e6!8m2!3d31.6293186!4d-7.9865762!16s%2Fg%2F11gg73wy49?entry=tts"
+    }
+]
