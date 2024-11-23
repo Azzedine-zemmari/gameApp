1aa3f8847f42dc9f66dd4a08de547faa6094f298
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-11-23 13:48:22 +0100

    complete a nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        GreenPrimary:"#2F4B4D",
+      },
+      fontFamily:{
+        primary:["Inter", "Inter Placeholder", 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
