1f04a6f02afec244a4b1b2d1722551b5863f9f86
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-11-24 12:13:42 +0100

    add cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,11 @@ module.exports = {
     extend: {
       colors:{
         GreenPrimary:"#2F4B4D",
-        secondery : "rgb(155, 96, 85)"
+        secondery : "rgb(155, 96, 85)",
       },
       fontFamily:{
-        primary:["Inter", "Inter Placeholder", 'sans-serif'],
-        secondery:["Instrument Serif", "Instrument Serif Placeholder", 'serif']
+        primary:["Inter", 'sans-serif'],
+        secondery:["Instrument Serif", 'serif']
       }
     },
   },
